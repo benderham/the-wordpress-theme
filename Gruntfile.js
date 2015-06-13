9394020cd1afae801e387ebb85c3609eed4da19d
Ben Derham <ben@benderham.com>
2015-06-13 19:09:38 +0800

    Initial Commit

---
+++ Gruntfile.js
@@ -0,0 +1,252 @@
+module.exports = function(grunt) {
+	
+	// Let's set up our 'source' and 'build' folders
+	var globalConfig = {
+    src: 'app/website-source',
+    build: 'app/wordpress/wp-content/themes/the-theme'
+  };
+	
+	// Load all grunt tasks matching the 'grunt-*' pattern
+	require('load-grunt-tasks')(grunt);
+	
+	// measures the time each task takes
+  require('time-grunt')(grunt);
+	
+	// Shall we dance?
+	grunt.initConfig({
+		globalConfig: globalConfig,
+		
+		// Creepy perve on the files and watch them changing... giggedy!
+		watch: {			
+			// Watch for any changes on the Gruntfile
+			gruntfile: {
+      	files: 'Gruntfile.js',
+				tasks: ['jshint:gruntfile']
+    	},
+			// Watch for changes in rootImages
+			rootImages: {
+				files: ['<%= globalConfig.src  %>/root-images/**/*.{jpg,jpeg,png,gif,svg}'],
+				tasks: ['newer:copy:rootImages']
+			},
+			// Watch for changes in themeTemplates
+			themeTemplates: {
+				files: ['<%= globalConfig.src  %>/theme-templates/**/*.{php,css,html}'],
+				tasks: ['newer:copy:themeTemplates']
+			},
+			// Watch for changes in fonts
+			fonts: {
+				files: ['<%= globalConfig.src  %>/fonts/**/*.{eot,svg,ttf,woff,woff2}'],
+				tasks: ['newer:copy:fonts']
+			},
+			// Watch for changes in vendor javascript
+			vendorScripts: {
+				files: ['<%= globalConfig.src  %>/js/vendor/**/*.js'],
+				tasks: ['concat:vendorScripts']
+			},
+			// Watch for changes in main javascript
+			mainScript: {
+				files: ['<%= globalConfig.src  %>/js/main.js'],
+				tasks: ['jshint:mainScript', 'copy:mainScript']
+			},
+			// Watch for changes in the images folder
+			themeImages: {
+				files: ['<%= globalConfig.src  %>/images/**/*.{jpg,jpeg,png,gif,svg}'],
+				tasks: ['newer:imagemin']
+			},
+			// Watch for changes to styles
+			styles: {
+				files: ['<%= globalConfig.src  %>/scss/**/*.{scss,sass}'],
+				tasks: ['sass:develop', 'autoprefixer']
+			}
+			
+		}, 
+		// end watching you filthy perve
+		
+		// Let's do some copying!
+		copy: {
+			// Copy Development & Build
+			
+			// Favicons and the theme's screenshot
+		  rootImages: {
+			  files: [{
+				  expand: true,
+				  cwd: '<%= globalConfig.src  %>/root-images/',
+				  src: ['**'], 
+				  dest: '<%= globalConfig.build %>/'
+				}]
+		  },
+		  
+		  // WordPress Theme Files - single.php, style.css etc
+		  themeTemplates: {
+			  files: [{
+				  expand: true,
+				  cwd: '<%= globalConfig.src  %>/theme-templates/',
+				  src: ['**'], 
+				  dest: '<%= globalConfig.build %>/'
+				}]
+		  },
+		  
+		  // Font files
+		  fonts: {
+			  files: [{
+				  expand: true,
+				  cwd: '<%= globalConfig.src  %>/fonts',
+				  src: ['**'], 
+				  dest: '<%= globalConfig.build %>/fonts'
+				}]
+		  },
+		  
+		  // Main javascript file
+		  mainScript: {
+			  files: [{
+				  expand: true,
+				  cwd: '<%= globalConfig.src  %>/js',
+				  src: ['main.js'],
+				  dest: '<%= globalConfig.build %>/js/'
+			  }]
+		  }
+					
+		}, 
+		// end copy
+		
+		// SASS/CSS Development
+		// Compile sass
+		sass: {
+			develop: {
+				options: {
+					style: 'expanded',
+					loadPath: require('node-bourbon').includePaths,
+          loadPath: require('node-neat').includePaths
+				},
+				files: {
+					'<%= globalConfig.build %>/css/main.css': '<%= globalConfig.src  %>/scss/main.scss'
+				}
+			}
+		},
+		// end compilation
+		
+		// Autoprefixer
+		autoprefixer: {
+			options: {
+				browsers: ['last 2 versions', 'ie9', 'ios 6', 'android 4']
+			},
+			files: {
+				expand: true,
+				flatten: true,
+				src: '<%= globalConfig.build %>/css/*.css'
+			}
+		},
+		// end autoprefixer
+		
+		// SASS/CSS Build
+		// Combine all of our media queries
+		cmq: {
+			mainCSS: {
+	        expand: true,
+	        src: '<%= globalConfig.build %>/css/*.css'
+	    }
+		}, // end combining media queries
+		
+		// Send our CSS to Jenny Craig
+		cssmin: {
+		  target: {
+		    files: [{
+		      expand: true,
+		      cwd: '<%= globalConfig.build %>/css/',
+		      src: ['*.css', '!*.min.css'],
+		      dest: '<%= globalConfig.build %>/css/',
+		      ext: '.css'
+		    }]
+		  }
+		},
+		// enough weight lost
+		
+		// Javascript Development
+		// Concat vendor/bower scripts
+		concat: {
+	    options: {
+	      separator: ';',
+	    },
+	    vendorScripts: {
+	      src: [
+	      	'<%= globalConfig.src  %>/js/vendor/vendor.js'
+	      ],
+	      dest: '<%= globalConfig.build %>/js/vendor.js',
+	    }
+	  },
+	  // end concat
+		
+		// Javascript linting using jshint
+		jshint: {
+			gruntfile: 'Gruntfile.js',
+			mainScript: '<%= globalConfig.src  %>/js/main.js'
+		}, 
+		// end linting
+		
+		// Javascript Build
+		// Javascript minification with uglify
+		uglify: {
+	    build: {
+	      files: {
+	        '<%= globalConfig.build %>/js/main.js': ['<%= globalConfig.build %>/js/main.js'],
+	        '<%= globalConfig.build %>/js/vendor.js': ['<%= globalConfig.build %>/js/vendor.js']
+	      }
+	    }
+	  }, 
+	  // end minification
+
+		// Get images ready for summer with imagemin
+		imagemin: {
+			themeImages: {
+				options: {
+					optimizationLevel: 7,
+					progressive: true,
+					interlaces: true
+				},
+				files: [{
+					expand: true,
+					cwd: '<%= globalConfig.src  %>/images/',
+					src: ['**/*.{jpg,jpeg,png,gif,svg}'],
+					dest: '<%= globalConfig.build %>/images/'
+				}]
+			}
+		},
+		// end imagemin
+		
+		// preview changes live with browserSync!
+		browserSync: {
+			dev: {
+				bsFiles: {
+					src: ['<%= globalConfig.build %>/css/main.css', '<%= globalConfig.build %>/js/*.js','<%= globalConfig.build %>/images/**/*.{jpg,jpeg,png,gif,svg}']
+				},
+				options: {
+					proxy: "dev.websiteurl.com",
+					watchTask: true
+				}
+			}
+		},
+		// end browserSync			
+	});	
+	
+	// Development
+	grunt.registerTask('default', [
+		'newer:copy',
+		'sass:develop', 'autoprefixer',
+		'jshint',
+		'concat:vendorScripts',
+		'newer:imagemin',
+		'browserSync', 'watch'
+	]);
+	
+	// Build
+	grunt.registerTask('build', [
+		'copy',
+		'sass:develop', 'autoprefixer',
+		'jshint',
+		'concat:vendorScripts',
+		'cmq','cssmin',
+		'uglify',
+		'imagemin'
+	]);
+	
+};
---
+++ README.md
@@ -0,0 +1,117 @@
+## Project Overview
+***
+Website built on a custom WordPress Template.
+
+### Development Team
+
+**Lead Developer**  
+Ben Derham - <ben@benderham.com>
+
+**Contributors**
+
+
+### Build Tools & Preprocessors
+
+**[SASS](http://sass-lang.com/guide)**  
+An extension of CSS that adds power and elegance to the basic language. It allows to use variables, nested rules, mixins, inline imports, and more.
+
+**[Grunt](http://gruntjs.com/getting-started)**  
+Automates tasks such as minification, concatenation, compression, compilation, browser refreshing etc.
+
+**[Bower](http://bower.io/#getting-started)**  
+Bower works by fetching and installing packages from all over, taking care of hunting, finding, downloading, and saving the stuff you’re looking for.
+
+**[WP-CLI](http://wp-cli.org)**  
+WP-CLI is a set of command-line tools for managing WordPress installations. You can update plugins, set up multisite installs and much more, without using a web browser.
+
+###Folder Structure
+	
+	wordpress-project/  
+	-- app/
+	-- bower_components/
+	-- node_modules/  
+	
+	---- website-source/
+	------ fonts/
+	------ images/
+	------ js/
+	-------- vendor/
+	------- scss/
+	------- root-images/
+	------- theme-templates/
+	
+	---- wordpress/
+	------ wp-content/
+	-------- themes/
+	---------- the-theme/
+
+### Important Files
+`Gruntfile.js`  
+Configuration of Grunt tasks.
+
+`package.json`  
+Configuration of node dependencies.
+
+`bower.json`  
+Configuration of bower dependencies.
+
+`wordpress.txt`  
+Details of the WordPress installation including: Version Number, Local Database Connection Details and Plugins. Use this file to determine the settings for your local installation. **Updated WordPress or installed a plugin?? UPDATE THIS FILE TOO!**
+
+
+#### JavaScript Libraries Used
+[jQuery](http://www.jquery.com)  
+[Modernizr](http://modernizr.com)  
+
+
+## Get Started
+***
+###STOP!
+Make sure that your machine has **ALL** of the Build Tools and Preprocessors set up **BEFORE** continuing further.
+
+###Create Working Folder and GIT Repository
+
+Clone this GIT Repository. [Don't know how?](https://confluence.atlassian.com/display/BITBUCKET/Clone+a+repository)
+
+
+###Local Development Environment 
+  
+Prepare a local development environment inside `longtail-site/app/wordpress/`, using your favourite local environment tool.   
+  
+***IMPORTANT: Only set up your Working Environment (Virtual Host, Folder Location, PHP Version) and Import Database. Install WordPress using WP-CLI and information in wordpress.txt in the next step.***
+
+[MAMP/MAMP Pro - OSX](http://codex.wordpress.org/Installing_WordPress_Locally_on_Your_Mac_With_MAMP)  
+[WampServer - Windows](https://make.wordpress.org/core/handbook/installing-a-local-server/installing-wampserver/)
+
+###Download & Configure WordPress
+
+Install WordPress using WP-CLI and the information on `wordpress.txt`
+
+	$ cd app/wordpress/
+	$ wp core download --version=x.xx
+	$ wp core config --dbname=database_name --dbuser=root --dbpass=password --dbhost=127.0.0.1
+	
+If for some reason configuring on the command line does not work, then go to `http://localhost/longtail-site` and configure using the WordPress GUI.
+
+###Run Grunt
+
+Run the default grunt task and start developing! 
+	
+	$ grunt
+
+The `Gruntfile.js` is commented throughout, each module used is well documented.
+
+
+## Important Notes
+***
+Only the contents of `longtail-project/ root` (except `bower_components/` and `node_modules/`) and `app/longtail-theme-source/` are kept under version control. This is so that WordPress and compiled files are not commited to the repository. 
+
+If you wish to preview and test the production version of the site you should run the Build Grunt Process. 
+
+	$ grunt build
+
+
+
+## Changelog
+***
+**v1.0** - Nothing has changed yet! We are only just getting this party started!
