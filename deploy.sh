4b0b9ec8d4d196d3739fb28454fcecbd4572df44
root <root@ip-172-31-10-122.ap-southeast-2.compute.internal>
2015-12-21 02:36:18 +0000

    Deployment script

--- deploy.sh
+++ deploy.sh
@@ -1,17 +1,19 @@
+#!/bin/bash
+
 # wordpress paths
-WP_PATH = '/home/Longtail/Websites/wordpress/'
+WP_PATH='/home/Longtail/Websites/wordpress/'
 
 # configuration
-PLUGINS = ('hello-dolly', 'contact-form-7')
-WP_VERSION = '4.4'
+PLUGINS=('hello-dolly' 'contact-form-7')
+WP_VERSION='4.4'
 
 # installing npm packages
-sudo npm install
-sudo bower install --allow-root
-sudo grunt build
+#sudo npm install
+#sudo bower install --allow-root
+#sudo grunt build
 
 #copy wp-content
-sudo cp -Rv app/wordpress/wp-content/ $WP_PATH
+#sudo cp -Rv app/wordpress/wp-content/ $WP_PATH
 
 #go to the worpress root folder
 cd $WP_PATH
@@ -19,9 +21,11 @@ cd $WP_PATH
 #check wordpress version
 echo 'Check WP version...'
 echo 'Current version is ' $(wp core version)
-if [$(wp core version) != '$WP_VERSION'] then
+if [ "$(wp core version)" != "$WP_VERSION" ]
+then
 	echo 'Updating Wordpress to the version:' $WP_VERSION
-	sudo wp core update --version=$WP_VERSION --force
+	sudo wp core update --version=$WP_VERSION --force --allow-root
+	echo 'Worpress has been updated to the version '$WP_VERSION
 else
 	echo 'Worpress version is up-to-date'
 fi
@@ -30,10 +34,10 @@ fi
 for i in "${PLUGINS[@]}"
 do
 	if ! $(wp plugin is-installed $i); then
-		echo 'Installing plugin $i'
-		sudo wp plugin is-installed $i --allow-root
+		echo 'Installing and activating plugin ' $i
+		sudo wp plugin install $i --activate --allow-root
 	else
-		echo 'Plugin $i is installed'
+		echo 'Plugin is installed: '$i
 	fi
 done
 
