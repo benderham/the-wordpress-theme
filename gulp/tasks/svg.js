3849e73ca0780a559f8b3ecfb07670a5b36e12fc
Luke Carl Thompson <info@lukethompson.com.au>
2019-03-24 17:06:57 +0800

    remove legacy gulp files

--- gulp/tasks/svg.js
+++
@@ -1,9 +0,0 @@
-var gulp = require('gulp');
-var svgSprite = require('gulp-svg-sprite');
-var config = require('../config');
-
-gulp.task('svg', function() {
-  return gulp.src(config.svg.src)
-    .pipe(svgSprite(config.svg))
-    .pipe(gulp.dest(config.svg.dest))
-});
