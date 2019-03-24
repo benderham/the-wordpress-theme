e7afb5b296360322a6f18f4b36863207fb0bdc29
Luke Carl Thompson <info@lukethompson.com.au>
2019-03-24 18:07:57 +0800

    remove oEmbed scripts

--- app/website-source/theme-templates/functions.php
+++ app/website-source/theme-templates/functions.php
@@ -106,6 +106,13 @@ function the_assets()
 remove_action('wp_head', 'print_emoji_detection_script', 7);
 remove_action('wp_print_styles', 'print_emoji_styles');
 
+// Remove oEmbed scripts
+// This scripts just helps with embedding other wordPress posts on your site, so it's not usually needed
+function my_deregister_scripts(){
+  wp_deregister_script( 'wp-embed' );
+}
+add_action( 'wp_footer', 'my_deregister_scripts' );
+
 /**
  * Theme Supports
  **/
