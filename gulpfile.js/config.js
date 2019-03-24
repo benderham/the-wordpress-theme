244029772ef1505cdda8347b34a473ce27b5ea56
Luke Carl Thompson <info@lukethompson.com.au>
2019-03-24 15:27:42 +0800

    removed modernizer and svg from config

--- gulpfile.js/config.js
+++ gulpfile.js/config.js
@@ -1,122 +1,81 @@
-var src = 'app/website-source';
-var build = 'app/wordpress/wp-content/themes/the-theme/';
+var src = "app/website-source";
+var build = "app/wordpress/wp-content/themes/the-theme/";
 
 module.exports = {
-
-browsersync: {
+  browsersync: {
     development: {
       // server: {
       //   baseDir: [build]
       // },
       proxy: {
         target: "http://the-wordpress-theme",
-        ws: true
+        ws: true,
       },
       port: 8080,
       files: [
-        build + '/css/*.css',
-        build + '/*.html',
-        build + '/images/**',
-        build + '/js/**/*.js'
-      ]
-    }
+        build + "/css/*.css",
+        build + "/*.html",
+        build + "/images/**",
+        build + "/js/**/*.js",
+      ],
+    },
   },
 
   scss: {
-    src:  src + '/scss/**/*.{sass,scss}',
-    dest: build + '/css/'
+    src: src + "/scss/**/*.{sass,scss}",
+    dest: build + "/css/",
   },
 
   scripts: {
-    src:  src + '/js/**/*.js',
-    dest: build + '/js/'
+    src: src + "/js/**/*.js",
+    dest: build + "/js/",
   },
 
   vendorScripts: {
     src: [],
-    dest: build + '/js/vendor'
+    dest: build + "/js/vendor",
   },
 
   autoprefixer: {
-    browsers: [
-      'last 2 versions',
-      'Safari >= 8',
-      'ie 8',
-      'ie 9'
-    ],
-    cascade: false
-  },
-
-  modernizr: {
-    src:  [
-      src + '/js/**/*.js',
-      src + '/scss/**/*.scss'
-    ],
-    dest: build + '/js/',
-    options: [
-      'setClasses',
-      'addTest',
-      'html5printshiv',
-      'testProp',
-      'fnBind'
-    ],
-    tests: [
-      'flexbox'
-    ]
+    browsers: ["last 2 versions", "Safari >= 8", "ie 8", "ie 9"],
+    cascade: false,
   },
 
   image: {
-    src: src + '/images/**/*.{jpg,png,gif,svg}',
-    dest: build + '/images/'
-  },
-
-  svg: {
-    src: src + '/svg/*.svg',
-    dest: build,
-    svg: {
-      xmlDeclaration: false,
-      doctypeDeclaration: false
-    },
-    mode: {
-      symbol: {
-        dest: './images',
-        sprite: 'svg-sprite.svg'
-      }
-    }
+    src: src + "/images/**/*.{jpg,png,gif,svg}",
+    dest: build + "/images/",
   },
 
   watch: {
-    sass: src + '/scss/**/*.{sass,scss}',
-    images:  src + '/images/**/*',
-    scripts: src + '/js/**/*',
-    templates: src + '/theme-templates/**/*',
-    svg: src + '/svg/**/*'
+    sass: src + "/scss/**/*.{sass,scss}",
+    images: src + "/images/**/*",
+    scripts: src + "/js/**/*",
+    templates: src + "/theme-templates/**/*",
   },
 
   favicons: {
-    src:  src + '/root-images/*',
-    dest: build
+    src: src + "/root-images/*",
+    dest: build,
   },
 
   templates: {
-    src:  src + '/theme-templates/**/*.{php,css,html}',
-    dest: build
+    src: src + "/theme-templates/**/*.{php,css,html}",
+    dest: build,
   },
 
   optimize: {
     css: {
-      src:  build + '/css/*.css',
-      dest: build + '/css/',
+      src: build + "/css/*.css",
+      dest: build + "/css/",
       options: {
-        keepSpecialComments: 0
-      }
+        keepSpecialComments: 0,
+      },
     },
 
     js: {
-      src:  build + '/js/*.js',
-      dest: build + '/js/',
-      options: {}
-    }
-  }
-
-}
+      src: build + "/js/*.js",
+      dest: build + "/js/",
+      options: {},
+    },
+  },
+};
